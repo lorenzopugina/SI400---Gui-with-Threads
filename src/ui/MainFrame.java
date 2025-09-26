6c6427da5c0c8076737e92f9b30b20646d746ebf
yuuosh <yuu.oshiro@gmail.com>
2025-09-25 23:37:25 -0300

    refac: Dialogs - Color, Pattern, Shape e Speed | mainFrame: add statusBar para cada dialog, att. variaveis em inglês

--- src/ui/MainFrame.java
+++ src/ui/MainFrame.java
@@ -58,12 +58,12 @@ public class MainFrame extends JFrame {
         JMenuBar menuBar = new JMenuBar();
 
         // menu arquivo ou submenu de arquivo
-        JMenu menuArquivo = new JMenu("Arquivo");// titulo do menu arquivos
-        JMenuItem abrir = new JMenuItem("Abrir Arquivo");
-        JMenuItem fechar = new JMenuItem("Fechar Arquivo");
-        JMenuItem sair = new JMenuItem("Sair");
+        JMenu menuArchive = new JMenu("Arquivo");// titulo do menu arquivos
+        JMenuItem open = new JMenuItem("Abrir Arquivo");
+        JMenuItem close = new JMenuItem("Fechar Arquivo");
+        JMenuItem exit = new JMenuItem("Sair");
 
-        abrir.addActionListener(e -> {
+        open.addActionListener(e -> {
             String content = FileManager.openFile(this); // delega ao FileManager
             if (content != null) {
                 textArea.setText(content);
@@ -72,54 +72,54 @@ public class MainFrame extends JFrame {
         });
 
 
-        fechar.addActionListener(e -> System.out.println("Clicou em Fechar Arquivo"));
+        close.addActionListener(e -> System.out.println("Clicou em Fechar Arquivo"));
         // quando o usuario clica aqui, aparece essa frase no terminal
-        sair.addActionListener(e -> System.exit(0)); // fecha a aplicacao/programa
+        exit.addActionListener(e -> System.exit(0)); // fecha a aplicacao/programa
 
-        menuArquivo.add(abrir);
-        menuArquivo.add(fechar);
-        menuArquivo.addSeparator();
-        menuArquivo.add(sair);
+        menuArchive.add(open);
+        menuArchive.add(close);
+        menuArchive.addSeparator();
+        menuArchive.add(exit);
 
         // menu configuracao ou submenu de configuracao
         JMenu menuConfig = new JMenu("Configuração");// titulo do menu configurações
-        JMenuItem padroes = new JMenuItem("Padrões");
-        JMenuItem cores = new JMenuItem("Cores");
-        JMenuItem velocidade = new JMenuItem("Velocidade");
-        JMenuItem formas = new JMenuItem("Forma");
+        JMenuItem colors = new JMenuItem("Cores");
+        JMenuItem shapes = new JMenuItem("Formas");
+        JMenuItem patterns = new JMenuItem("Padrões");
+        JMenuItem speed = new JMenuItem("Velocidade");
 
         //erros pq ainda n foram implementados
-        velocidade.addActionListener(e -> new SpeedDialog(this).setVisible(true));
-        cores.addActionListener(e -> new ColorDialog(this).setVisible(true));
-        formas.addActionListener(e -> new ShapeDialog(this).setVisible(true));
-        padroes.addActionListener(e -> new PatternDialog(this).setVisible(true));
+        speed.addActionListener(e -> new SpeedDialog(this, statusBar).setVisible(true));
+        colors.addActionListener(e -> new ColorDialog(this, statusBar).setVisible(true));
+        shapes.addActionListener(e -> new ShapeDialog(this, statusBar).setVisible(true));
+        patterns.addActionListener(e -> new PatternDialog(this, statusBar).setVisible(true));
 
-        menuConfig.add(velocidade);
-        menuConfig.add(cores);
-        menuConfig.add(formas);
-        menuConfig.add(padroes);
+        menuConfig.add(speed);
+        menuConfig.add(colors);
+        menuConfig.add(shapes);
+        menuConfig.add(patterns);
 
         // Manu Ajuda ou submenu de ajuda
-        JMenu menuAjuda = new JMenu("Ajuda"); // titulo do menu ajuda
-        JMenuItem ajuda = new JMenuItem("Ajuda");
-        JMenuItem sobre = new JMenuItem("Sobre");
+        JMenu menuHelp = new JMenu("Ajuda"); // titulo do menu ajuda
+        JMenuItem help = new JMenuItem("Ajuda");
+        JMenuItem about = new JMenuItem("Sobre");
 
-        ajuda.addActionListener(e -> {
+        help.addActionListener(e -> {
             HelpDialog helpDialog = new HelpDialog(this);
             helpDialog.setVisible(true); // mostra o conteudo de ajuda
         });
 
-        sobre.addActionListener(e -> {
+        about.addActionListener(e -> {
             AboutDialog aboutDialog = new AboutDialog(this);
             aboutDialog.setVisible(true);// mostra o conteudo de sobre
         });
 
-        menuAjuda.add(ajuda);
-        menuAjuda.add(sobre);
+        menuHelp.add(help);
+        menuHelp.add(about);
 
-        menuBar.add(menuArquivo);
+        menuBar.add(menuArchive);
         menuBar.add(menuConfig);
-        menuBar.add(menuAjuda);
+        menuBar.add(menuHelp);
 
         return menuBar;
     }
--- src/ui/dialogs/PatternDialog.java
+++ src/ui/dialogs/PatternDialog.java
@@ -1,5 +1,32 @@
 package ui.dialogs;
 
-public class PatternDialog {
-    //parecido com color e speed
+import core.ConfigManager;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+public class PatternDialog extends JDialog{
+    public PatternDialog(JFrame parent, JLabel statusBar) {
+        super(parent, "Restaurar Padrão", true);
+        setSize(350, 200);
+        setLocationRelativeTo(parent);
+
+        JLabel label = new JLabel("<html><center>Deseja restaurar as configurações para o padrão?</center></html>");
+        JButton ok = new JButton("OK");
+        
+        ok.addActionListener( e -> {
+            ConfigManager.resetDefaults();
+            statusBar.setText("Configurações resetadas para o padrão.");
+            dispose();
+        });
+
+        JPanel panel = new JPanel (new BorderLayout());
+        panel.add(label, BorderLayout.CENTER);
+        panel.add(ok, BorderLayout.SOUTH);
+        add(panel);
+    }
 }
